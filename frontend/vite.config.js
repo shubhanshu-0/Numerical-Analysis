44b548d3ae54c1fd43781ccc14f169b0cf818c24
shubhanshu-0 <shubhanshusaraf0@gmail.com>
2024-12-13 07:15:47 +0530

    proxy fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
 dotenv.config();
 
-// https://vite.dev/config/
+//https://vite.dev/config/
 export default defineConfig({
   server : {
     proxy : {
       '/api' : {
         target : process.env.URL ,
+        changeOrigin: true
       }
     } ,
   } ,
